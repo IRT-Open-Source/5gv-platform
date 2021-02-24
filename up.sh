6bd267c5a9218d416fb2ec27394161fa7754b82e
Christoph Ziegler <christophziegler@hotmail.de>
2021-02-24 13:40:48 +0100

    Fix issue #6

--- up.sh
+++ up.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ ! $1 ]; then
-    echo "Missing expected argument 'private key name'"
-    exit 1
-fi
-
 work_dir=$(pwd)
 echo $work_dir
 
@@ -38,14 +33,7 @@ while read -r repo_url; do
     echo "Removing potentially running Docker images"
     docker rmi $repo_name
 
-    # Build docker images
-    # 'DOCKER_BUILDKIT=1' tells the docker daemon to activate experimental features.
-    # Needed to enable the '--ssh' argument, which allows passing the location of the
-    # private key needed to authenticate at the private GitLab repository to the docker
-    # daemon, which will temporaly mount the respective file to be available during
-    # the build process.
-    # (More info at: https://sanderknape.com/2019/06/installing-private-git-repositories-npm-install-docker/)docker
-    DOCKER_BUILDKIT=1 docker build --ssh gitlab="$HOME/.ssh/$1" -t $repo_name .
+    docker build -t $repo_name .
 
     # Go back to 'work_dir'
     cd $work_dir
